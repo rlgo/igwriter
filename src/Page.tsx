57514e7db94e637be7ca5cfa2726333728cf63ce
rlgo <alexfang10@gmail.com>
2021-01-10 03:09:13 +0800

    file title

--- src/Page.tsx
+++ src/Page.tsx
@@ -19,7 +19,7 @@ function Topbar({ path }: TopbarProps) {
 
   return (
     <Box bg={blue} w="100%" p={padding} paddingTop="0.7rem" paddingBottom="0.7rem">
-      <HStack justify="space-between" maxW={maxWidth} m="auto">
+      <HStack justify="space-between" paddingTop="0.6rem" maxW={maxWidth} m="auto">
         <HStack spacing={spacing}>
           <Avatar size="sm" src={user?.photoURL || ""} />
           <Text color="white" fontSize="1.2rem" fontWeight="500">{uppercase(path)}</Text>
@@ -33,7 +33,7 @@ function Topbar({ path }: TopbarProps) {
   )
 
   function login() {
-    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider())
     // firebase.auth().signOut();
   }
 }
@@ -43,13 +43,18 @@ type FilesProps = {
 }
 
 function Files({ path }: FilesProps) {
+  const titles = {
+    [Path.HOME]: "Recent Files",
+    [Path.FAVORITES]: "Favorites Files",
+    [Path.SHARED]: "Shared Files"
+  }
+  const title = titles[path]
+
   return (
-    <Box p={padding} w="100vw">
-      <Hint type={Path.HOME} />
-      <br></br>
-      <Hint type={Path.FAVORITES} />
-      <br></br>
-      <Hint type={Path.SHARED} />
+    <Box maxW={maxWidth} m="auto">
+      <Text mb="0.8rem">{title}</Text>
+      <Hint type={path} />
+      <br />
     </Box>
   )
 
@@ -61,7 +66,7 @@ function Files({ path }: FilesProps) {
       [Path.FAVORITES]: "Tap the \u22EE icon next to your draft and mark any draft as favorite to access it faster in the future.",
       [Path.SHARED]: "Invite your friends or enemies to collaborate on the draft that you currently work on to boost productivity."
     }
-    return <Box fontSize={size} maxW={maxWidth} color="GrayText" m="auto" bg={lightgrey} p={padding} rounded="md">
+    return <Box fontSize={size} color="GrayText" m="auto" bg={lightgrey} p={padding} rounded="md">
       {description[type]}</Box>
   }
 }
@@ -74,7 +79,12 @@ export default function Page({ path }: PageProps) {
   return (
     <VStack>
       <Topbar path={path} />
-      <Files path={path} />
+      <Box p={padding} w="100vw">
+        <Files path={path} />
+        {path === Path.HOME
+          ? <><Files path={Path.FAVORITES} /> <Files path={Path.SHARED} /></>
+          : <></>}
+      </Box>
     </VStack>
   )
 }
